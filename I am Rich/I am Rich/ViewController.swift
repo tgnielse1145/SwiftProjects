ff36b9b3f7eb5259acf5eb64cd3ef0edfc9ad667
tgnielse1145 <tgnielse1145@gmail.com>
2022-05-03 10:21:25 -0600

    first commit

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Todd Nielsen on 5/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
