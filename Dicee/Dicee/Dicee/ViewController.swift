5c6fcc781f8f504d8f73a9ff2808d21cbb95b000
tgnielse1145 <tgnielse1145@gmail.com>
2022-05-03 18:54:58 -0600

    magic 8 ball

--- Dicee/Dicee/Dicee/ViewController.swift
+++ Dicee/Dicee/Dicee/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //diceImageView1.image = UIImage(named: "DiceSix")
+        //diceImageView2.image = UIImage(named: "DiceFive")
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+       // diceImageView1.image=UIImage(named: "DiceThree")
+        //diceImageView2.image=UIImage(named: "DiceFour")
+       
+        
+//        diceImageView1.image = UIImage(named:diceArray[leftDiceNumber])
+//        diceImageView2.image = UIImage(named:diceArray[rightDiceNumber])
+//
+//
+//        leftDiceNumber += 1
+//        rightDiceNumber -= 1;
+//
+//        if(leftDiceNumber==6){
+//            leftDiceNumber=0;
+//        }
+//        if(rightDiceNumber < 0){
+//            rightDiceNumber=5
+//        }
+        let diceArray = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
+//        diceImageView1.image = UIImage(named:diceArray[Int.random(in: 0...5)])
+        diceImageView1.image = UIImage(named: diceArray.randomElement()!)
+        
+        diceImageView2.image = UIImage(named:diceArray[Int.random(in: 0...5)])
+       
+    }
+    
 }
 
