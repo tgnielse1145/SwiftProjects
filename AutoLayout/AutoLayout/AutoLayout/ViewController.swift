277965bf1282c465575c4d2eaa283909deda97c0
tgnielse1145 <tgnielse1145@gmail.com>
2022-05-03 22:50:44 -0600

    AutoLayout Alignment

--- AutoLayout/AutoLayout/AutoLayout/ViewController.swift
+++ AutoLayout/AutoLayout/AutoLayout/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        let diceArray = ["DiceOne","DiceTwo","DiceThree","DiceFour","DiceFive","DiceSix"]
+        //        diceImageView1.image = UIImage(named:diceArray[Int.random(in: 0...5)])
+                diceImageView1.image = UIImage(named: diceArray.randomElement()!)
+                
+                diceImageView2.image = UIImage(named:diceArray[Int.random(in: 0...5)])
+               
+    }
 }
 
